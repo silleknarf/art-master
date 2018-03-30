39f6adb8a803572c582033b293b5cb7edc438942
silleknarf@gmail.com <silleknarf@gmail.com>
2018-03-30 19:12:12 +0100

    Updated service

--- service/src/service.py
+++ service/src/service.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
 
 import sys
-sys.path.append("../database")
-from database import Database, User
-from flask import Flask, request
+from database.database import Database
+from database.data_model import User, Round, Image, RoundImage, Rating
+from flask import Flask, request, jsonify
 
 data_dir = "/Users/silleknarf/Code/art-master/data"
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "Welcome to the art-master api"
+
 # Creates a new user if the name isn't already taken
 @app.route("/user/<string:username>", methods=["POST"])
 def create_user(username):
     sesh = Database().get_session()
-    existing_user = sesh.query(User).filter(Username=username).first()
+    existing_user = (sesh.query(User)
+        .filter(User.Username==username).first())
     if existing_user is None:
         new_user = User(Username=username)
         sesh.add(new_user)
         sesh.commit()
-        return new_user
-    return None
+        return jsonify({
+                "userId": new_user.UserId,
+                "username": new_user.Username
+            })
+    return jsonify({})
 
 # Create a new round and then returns the round info
 @app.route("/round", methods=["POST"])
@@ -28,10 +37,10 @@ def create_round():
     round = Round()
     sesh.add(round)
     sesh.commit()
-    return round
+    return jsonify({ "roundId": round.RoundId })
 
 #provide the image in the body
-@app.route("round/<int:round_id>/user/<int:user_id>/drawing", methods=["POST"])
+@app.route("/round/<int:round_id>/user/<int:user_id>/drawing", methods=["POST"])
 def add_drawing(round_id, user_id):
     drawing_file = request.files("drawing")
     drawing_file_name = round_id + "/" + user_id + ".png"
@@ -42,18 +51,25 @@ def add_drawing(round_id, user_id):
     sesh.flush()
     round_drawing = RoundImage(RoundId=round_id, ImageId=drawing.ImageId)
     sesh.commit()
+    return ({ 
+        "imageId": drawing.ImageId,
+        "roundImageId": round_drawing.RoundImageId
+    })
 
 # returns two images to rate
-@app.route("round/<int:round_id>/drawings", methods=["GET"])
+@app.route("/round/<int:round_id>/drawings", methods=["GET"])
 def get_drawings(round_id):
     sesh = Database().get_session()
-    round_images = sesh.query(RoundImage).filter(RoundId==round_id).all()
+    round_images = (sesh
+        .query(RoundImage)
+        .filter(RoundImage.RoundId==round_id)
+        .all())
     locations = [ri.Image.Location for ri in round_images]
-    return locations
+    return jsonify(locations)
 
 # provides the round info including who won
-@app.route("round/<int:round>", methods=["GET"])
-def get_round_info():
+@app.route("/round/<int:round_id>", methods=["GET"])
+def get_round_info(round_id):
     sesh = Database().get_session()
     round_ratings = (sesh
         .query(Rating) 
@@ -75,26 +91,29 @@ def get_round_info():
             winner_image_id = image_id
             winner_rating = rating
 
-    winning_image_location = (round_ratings
-        .filter(lambda rr: rr.RoundImage.ImageId == winner_image_id)[0]
-        .Location)
+    winning_image_location = filter(
+        lambda rr: rr.RoundImage.ImageId == winner_image_id,
+        round_ratings)[0].Location
 
     round_info = { 
         "round_id": round_id,
         "winner_id": winner_id,
         "winning_image_location": winning_image_location
     }
-    return round_info 
+    return jsonify(round_info)
 
 # provide the rating
-@app.route("drawing/<int:image_id>/", methods=["POST"])
-def provide_ratiing(round_id, image_id):
+@app.route("/drawing/<int:image_id>/", methods=["POST"])
+def provide_rating(round_id, image_id):
     rating = request.args.get("rating")
     user_id = request.args.get("raterUserId")
-    round_entity = Rating(Rating=rating, RaterUserId=raterUserId, ImageId=image_id)
+    rating_entity = Rating(Rating=rating, RaterUserId=raterUserId, ImageId=image_id)
     sesh = Database().get_session()
-    sesh.add(round_entity)
+    sesh.add(rating_entity)
     sesh.commit()
+    return jsonify({
+        "ratingId", rating_entity.RatingId
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
