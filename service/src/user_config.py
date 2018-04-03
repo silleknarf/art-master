d13dd244af4917561ed35ea1b35322ca07c638bd
silleknarf@gmail.com <silleknarf@gmail.com>
2018-04-03 23:39:35 +0100

    Added a database setup script

--- service/src/user_config.py
+++ service/src/user_config.py
@@ -5,8 +5,8 @@ from public_config import DevelopmentConfig, ProductionConfig
 class UserDevelopmentConfig(DevelopmentConfig):
     # Set you details for your local
     # database configuration
-    DATABASE_USERNAME = "root"
-    DATABASE_PASSWORD = "glad0sglad0s"
+    DATABASE_USERNAME = ""
+    DATABASE_PASSWORD = ""
     DATABASE_NAME = "art-master-dev"
 
 class PrivateProductionConfig(ProductionConfig):
