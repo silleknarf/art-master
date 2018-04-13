e7a8275683dc5e104cd7a215a8fe0a07ace50883
Frank Ellis <silleknarf@gmail.com>
2018-04-13 16:18:06 +0100

    Implemented upload drawing button

--- service/src/services/image_service.py
+++ service/src/services/image_service.py
@@ -4,18 +4,22 @@ from flask import Blueprint, jsonify, request
 
 from database.database import session
 from database.data_model import Image
+import base64
 
 image_service = Blueprint('image_service', __name__)
+data_dir = "../../data"
 
 #provide the image in the body
 @image_service.route("/image", methods=["POST"])
 def add_drawing():
     user_id = int(request.args.get("userId"))
     round_id = int(request.args.get("roundId"))
-    drawing_file = request.files("drawing")
+    drawing_base64 = request.form["drawingBase64"]
 
     drawing_file_name = round_id + "/" + user_id + ".png"
-    f.save(data_dir + "/" + drawing_file_name);
+    drawing_file_path = data_dir + "/" + drawing_file_name
+    with open(drawing_file_path, "wb") as fh:    
+      fh.write(base64.decodebytes(drawing_base64))
     drawing = Image(
         UserId=user_id, 
         Location=drawing_file_name, 
--- ui/src/components/common/Draw.js
+++ ui/src/components/common/Draw.js
@@ -7,16 +7,43 @@ class Draw extends Component {
 
   constructor(props) {
     super(props);
+    const { userId, roundIdd } = props;
     this.state = {
-      drawing: null,
+      userId: userId,
+      roundId: roundId
+    }
+  }
+  
+  async onClickUploadDrawing(e) {
+    const drawingDataUrl = this.canvas.toDataURL();
+    const drawingRes = await fetch(`${config.apiurl}/image?userId=${this.state.userId}&roundId=${this.state.roundId}`, {
+        method: 'POST',
+        body: JSON.stringify({"drawingBase64": drawingDataUrl})
+      });
+    if (res.status === 200) {
+      console.log("Uploaded drawing")
     }
   }
 
   render() {
     return (
       <div className="draw">
-        <canvas id="draw-canvas">
-        </canvas>
+        <Row>
+          <Col>
+            <canvas id="draw-canvas" ref={(c) => this.canvas = c}>
+            </canvas>
+          </Col>
+        </Row>
+        <Row className="button-row">
+          <Col smOffset={3} sm={6}>
+            <Button
+              className="upload-room-button button"
+              onClick={(e) => this.onClickUploadDrawing(e)}
+            >
+              Upload Drawing
+            </Button>
+          </Col>
+        </Row>
       </div>
     );
   }
