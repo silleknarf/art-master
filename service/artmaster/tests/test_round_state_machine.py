a792abfd85bea83da842e728efa57aefde5fc8c5
Frank Ellis <silleknarf@gmail.com>
2018-05-10 18:36:37 +0100

    Added a test for the transition from drawing to critiquing

--- service/artmaster/tests/test_round_state_machine.py
+++ service/artmaster/tests/test_round_state_machine.py
@@ -3,8 +3,9 @@
 import unittest
 import mock
 import json
-from services.round_state_machine import RoundStateMachine
+from services.round_state_machine import RoundStateMachine, RoundState
 from test_utils import *
+from datetime import datetime
 
 class TestRoundStateMachine(unittest.TestCase):
     round = {
@@ -22,6 +23,25 @@ class TestRoundStateMachine(unittest.TestCase):
         round_state_machine = RoundStateMachine(round_entity)
         round_state_machine.next_stage()
         round_repository.update_room_round.assert_called_once()
+        round_repository.update_round.assert_called_once()
+
+   @mock.patch("services.round_state_machine.datetime")
+    @mock.patch("services.round_state_machine.round_repository")
+    def test_go_through_rounds(self, round_repository, mock_datetime):
+        round_entity = Struct(**self.round)
+        start_time = datetime.datetime(2018, 05, 01, 12, 0, 0)
+        mock_datetime.utc_now.return_value = start_time
+        round_state_machine = RoundStateMachine(round_entity)
+        round_state_machine.next_stage()
+        round_state_machine.next_stage()
+        expected_end_time = datetime.datetime(2018, 05, 01, 12, 0, 30)
+        round_repository.update_round.assert_called_with(
+            round_entity.RoundId,
+            RoundState.CRITIQUING,
+            start_time,
+            expected_end_time,
+            round_entity.DrawingWordId)
+        round_state_machine.next_stage()
         round_repository.update_round.assert_called_once()
 
 if __name__ == '__main__':
