e961c8be337dde81f8f5b4644c38b46855f55dcb
silleknarf@gmail.com <silleknarf@gmail.com>
2020-11-16 21:34:50 +0000

    Renamed database to be called craicbox

--- service/artmaster/artmaster/config.py
+++ service/artmaster/artmaster/config.py
@@ -7,6 +7,6 @@ class Config(object):
     URL = os.getenv("CRAICBOX_URL")
     DATABASE_SERVER = "db:3306"
     DATABASE_USERNAME = "root"
-    DATABASE_NAME = "art-master-dev"
+    DATABASE_NAME = "craicbox"
     DATABASE_PASSWORD = os.getenv("CRAICBOX_DATABASE_PASSWORD")
     SOCKETIO_PASSWORD = os.getenv("SOCKETIO_PASSWORD")
