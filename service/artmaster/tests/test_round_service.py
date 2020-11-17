dc86761861a47d56e812e7e1789c21401220b455
silleknarf@gmail.com <silleknarf@gmail.com>
2020-11-17 22:02:28 +0000

    Fix get or create round test

--- service/artmaster/tests/test_round_service.py
+++ service/artmaster/tests/test_round_service.py
@@ -6,6 +6,7 @@ import json
 import app
 from services import round_service
 from .test_utils import *
+from flask import Response
 
 class TestRoundService(unittest.TestCase):
     round = {
@@ -21,17 +22,25 @@ class TestRoundService(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
+    @mock.patch("services.round_service.to_round_dto")
+    @mock.patch("services.round_service.run_round")
     @mock.patch("services.round_service.RoundStateMachine")
     @mock.patch("services.round_service.word_repository")
     @mock.patch("services.round_service.round_repository")
-    def test_get_or_create_round(self, round_repository, word_repository, round_state_machine):
+    def test_get_or_create_round(
+        self,
+        round_repository,
+        word_repository,
+        round_state_machine,
+        run_round,
+        to_round_dto):
         round_repository.get_or_create_round.return_value = Struct(**self.round)
-        mock_state_machine = mock.Mock()
-        mock_state_machine.get_time_remaining.return_value = 30
-        round_state_machine.return_value = mock_state_machine
+        round_state_machine.get_time_remaining.return_value = 30
+        to_round_dto.return_value = Response()
+
         self.app.post("/round?roomId=1&userId=1")
-        round_repository.get_or_create_round.assert_called_once()
-        mock_state_machine.next_stage.assert_called_once()
+        round_repository.create_round.assert_called_once()
+        run_round.apply_async.assert_called_once()
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRoomService)
