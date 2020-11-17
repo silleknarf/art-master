66d334b3927f01d7fe3abadc00c580a5494367bd
silleknarf@gmail.com <silleknarf@gmail.com>
2020-11-17 01:35:05 +0000

    Fix API smoke test

--- service/artmaster/tests/test_app.py
+++ service/artmaster/tests/test_app.py
@@ -9,5 +9,5 @@ class TestApp(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_home(self):
-        expected_string = b"Welcome to the Craicbox api"
+        expected_string = b"Welcome to the Craicbox API"
         self.assertEqual(self.app.get("/").data, expected_string)
